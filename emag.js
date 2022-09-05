c64f40649f4240accdfba87c2e0f93c657086f59
Cimpan Cristian <112417929+CristianCimpan@users.noreply.github.com>
2022-09-05 12:37:23 +0300

    Add files via upload

---
+++ emag.js
@@ -0,0 +1,356 @@
+
+var meniu = document.querySelector(".openmenu");
+console.log(meniu);
+
+var meniuLateral = document.querySelector(".sidemenu");
+console.log(meniuLateral);
+
+var banner = document.querySelector(".baner");
+console.log(banner);
+const buton = document.querySelectorAll(".but");
+
+var meniuPrincipal = document.querySelector(".menu");
+/*********************ASCUNDE Meniu Lateral 2******** */
+var afiseazaMeniuM = document.querySelectorAll(".side_block1 p");
+var afiseazaMeniuMArr = Array.from(afiseazaMeniuM);
+var afiseazaMeniuP = document.querySelectorAll(".side_block1");
+var afiseazaMeniuPArr = Array.from(afiseazaMeniuP);
+console.log(afiseazaMeniuPArr)
+var afiseazaMeniuMs = document.querySelectorAll(".side_block1 h3");
+
+
+console.log(afiseazaMeniuM);
+function afiseazaMeniuMobil() {
+  for (var i = 0; i <= afiseazaMeniuM.length; i++) {
+
+    if (afiseazaMeniuMArr[i].style.display === "none" || afiseazaMeniuMArr[i].style.display == '') {
+      afiseazaMeniuMArr[i].style.display = "flex";
+    } else {
+      afiseazaMeniuMArr[i].style.display = "none";
+    }
+  }
+}
+/*afiseazaMeniuPArr.forEach(function (buton2, index, sirComplet) {
+buton2.addEventListener("click", afiseazaMeniuMobil)
+}) */
+var afiseazaMeniuM2 = document.querySelectorAll(".side_block2 p");
+var afiseazaMeniuMArr2 = Array.from(afiseazaMeniuM2);
+var afiseazaMeniuP2 = document.querySelectorAll(".side_block2");
+var afiseazaMeniuPArr2 = Array.from(afiseazaMeniuP2);
+var afiseazaMeniuMs2 = document.querySelectorAll(".side_block2 h3");
+
+
+
+function afiseazaMeniuMobil2() {
+  for (var i = 0; i <= afiseazaMeniuM2.length; i++) {
+
+    if (afiseazaMeniuMArr2[i].style.display === "none" || afiseazaMeniuMArr2[i].style.display == '') {
+      afiseazaMeniuMArr2[i].style.display = "flex";
+    } else {
+      afiseazaMeniuMArr2[i].style.display = "none";
+    }
+  }
+}
+/********************************** */
+
+var afiseazaMeniuM4 = document.querySelectorAll(".side_block4 p");
+var afiseazaMeniuMArr4 = Array.from(afiseazaMeniuM4);
+var afiseazaMeniuP4 = document.querySelectorAll(".side_block4");
+var afiseazaMeniuPArr4 = Array.from(afiseazaMeniuP4);
+var afiseazaMeniuMs4 = document.querySelectorAll(".side_block4 h3");
+
+
+
+function afiseazaMeniuMobil4() {
+  for (var i = 0; i <= afiseazaMeniuM4.length; i++) {
+
+    if (afiseazaMeniuMArr4[i].style.display === "none" || afiseazaMeniuMArr4[i].style.display == '') {
+      afiseazaMeniuMArr4[i].style.display = "flex";
+    } else {
+      afiseazaMeniuMArr4[i].style.display = "none";
+    }
+  }
+}
+/************************************************ */
+var afiseazaMeniuM5 = document.querySelectorAll(".side_block5 p");
+var afiseazaMeniuMArr5 = Array.from(afiseazaMeniuM5);
+var afiseazaMeniuP5 = document.querySelectorAll(".side_block5");
+var afiseazaMeniuPArr5 = Array.from(afiseazaMeniuP5);
+var afiseazaMeniuMs5 = document.querySelectorAll(".side_block5 h3");
+
+
+
+function afiseazaMeniuMobil5() {
+  for (var i = 0; i <= afiseazaMeniuM5.length; i++) {
+
+    if (afiseazaMeniuMArr5[i].style.display === "none" || afiseazaMeniuMArr5[i].style.display == '') {
+      afiseazaMeniuMArr5[i].style.display = "flex";
+    } else {
+      afiseazaMeniuMArr5[i].style.display = "none";
+    }
+  }
+}
+/************************************************ */
+var afiseazaMeniuM6 = document.querySelectorAll(".side_block6 p");
+var afiseazaMeniuMArr6 = Array.from(afiseazaMeniuM6);
+var afiseazaMeniuP6 = document.querySelectorAll(".side_block6");
+var afiseazaMeniuPArr6 = Array.from(afiseazaMeniuP6);
+var afiseazaMeniuMs6 = document.querySelectorAll(".side_block6 h3");
+
+
+
+function afiseazaMeniuMobil6() {
+  for (var i = 0; i <= afiseazaMeniuM6.length; i++) {
+
+    if (afiseazaMeniuMArr6[i].style.display === "none" || afiseazaMeniuMArr6[i].style.display == '') {
+      afiseazaMeniuMArr6[i].style.display = "flex";
+    } else {
+      afiseazaMeniuMArr6[i].style.display = "none";
+    }
+  }
+}
+/************************************************ */
+var afiseazaMeniuM7 = document.querySelectorAll(".side_block7 p");
+var afiseazaMeniuMArr7 = Array.from(afiseazaMeniuM7);
+var afiseazaMeniuP7 = document.querySelectorAll(".side_block7");
+var afiseazaMeniuPArr7 = Array.from(afiseazaMeniuP7);
+var afiseazaMeniuMs7 = document.querySelectorAll(".side_block7 h3");
+
+
+
+function afiseazaMeniuMobil7() {
+  for (var i = 0; i <= afiseazaMeniuM7.length; i++) {
+
+    if (afiseazaMeniuMArr7[i].style.display === "none" || afiseazaMeniuMArr7[i].style.display == '') {
+      afiseazaMeniuMArr7[i].style.display = "flex";
+    } else {
+      afiseazaMeniuMArr7[i].style.display = "none";
+    }
+  }
+}
+/************************************************ */
+var afiseazaMeniuM8 = document.querySelectorAll(".side_block8 p");
+var afiseazaMeniuMArr8 = Array.from(afiseazaMeniuM8);
+var afiseazaMeniuP8 = document.querySelectorAll(".side_block8");
+var afiseazaMeniuPArr8 = Array.from(afiseazaMeniuP8);
+var afiseazaMeniuMs8 = document.querySelectorAll(".side_block8 h3");
+
+
+
+function afiseazaMeniuMobil8() {
+  for (var i = 0; i <= afiseazaMeniuM8.length; i++) {
+
+    if (afiseazaMeniuMArr8[i].style.display === "none" || afiseazaMeniuMArr8[i].style.display == '') {
+      afiseazaMeniuMArr8[i].style.display = "flex";
+    } else {
+      afiseazaMeniuMArr8[i].style.display = "none";
+    }
+  }
+}
+/************************************************ */
+var afiseazaMeniuM9 = document.querySelectorAll(".side_block9 p");
+var afiseazaMeniuMArr9 = Array.from(afiseazaMeniuM9);
+var afiseazaMeniuP9 = document.querySelectorAll(".side_block9");
+var afiseazaMeniuPArr9 = Array.from(afiseazaMeniuP9);
+var afiseazaMeniuMs9 = document.querySelectorAll(".side_block9 h3");
+
+
+
+function afiseazaMeniuMobil9() {
+  for (var i = 0; i <= afiseazaMeniuM9.length; i++) {
+
+    if (afiseazaMeniuMArr9[i].style.display === "none" || afiseazaMeniuMArr9[i].style.display == '') {
+      afiseazaMeniuMArr9[i].style.display = "flex";
+    } else {
+      afiseazaMeniuMArr9[i].style.display = "none";
+    }
+  }
+}
+/**************************** */
+
+
+
+
+function afiseazaMeniu() {
+  if (meniuPrincipal.style.display == "none" || meniuPrincipal.style.display == '') {
+    meniuPrincipal.style.display = "flex";
+  } else {
+    meniuPrincipal.style.display = "none";
+  }
+}
+
+
+function reseteaza() {
+  if (window.innerWidth >= 820) {
+    meniuPrincipal.style.display = "flex";
+  }
+  else {
+    meniuPrincipal.style.display = "none";
+  }
+}
+window.addEventListener("resize", reseteaza)
+
+
+function afiseaza() {
+  meniuLateral.style.display = "flex"
+  banner.style.display = "none";
+  if (window.innerWidth <= 820) {
+    meniuLateral.style.display = "none";
+    banner.style.display = "flex";
+  }
+
+}
+
+
+function ascunde() {
+  meniuLateral.style.display = "none";
+  banner.style.display = "flex";
+}
+
+var afiseazaSide = document.querySelector(".sideMobil");
+var MeniulateralMobil = document.querySelector(".meniulateral")
+
+console.log(afiseazaSide);
+
+function afiseazaSideMenu() {
+  if (window.innerWidth <= 820) {
+    afiseazaSide.style.display = "inline-block";
+    MeniulateralMobil.style.display = "none";
+
+  }
+  else {
+    afiseazaSide.style.display = "none";
+  }
+}
+
+function reseteazaSideMenu() {
+  if (window.innerWidth >= 820) {
+    afiseazaSide.style.display = "none";
+    MeniulateralMobil.style.display = "inline-block";
+
+  }
+}
+window.addEventListener("resize", reseteazaSideMenu)
+
+
+
+
+function ascundeSideMenu() {
+  if (window.innerWidth <= 820) {
+    afiseazaSide.style.display = "none";
+    MeniulateralMobil.style.display = "inline-block";
+
+  }
+  else {
+    afiseazaSide.style.display = "none";
+  }
+}
+/*  function afiseazaDescriere() {
+  
+    for (let i = 0; i < description1.length; i++) {
+      description1[i].style.display = "flex";
+    }
+  } */
+
+function afiseazaDescriere2(elementSelectat) {
+  console.log(elementSelectat)
+  const cardParent = elementSelectat.target.parentElement;
+  const description = cardParent.querySelector(".descriere");
+  console.log(description);
+  if (description.style.display === "none" || description.style.display == '') {
+    description.style.display = "block";
+  } else {
+    description.style.display = "none";
+  }
+
+
+}
+
+/**************PArte testimonial */
+buton.forEach(function (buton2, index, sirComplet) {
+  buton2.addEventListener("click", afiseazaDescriere2)
+})
+
+
+/**************inserare poze */
+
+const pozeOffice = [{ numefisier: "1.webp", numeProdus: "Ofiice Profesional", pret: "400 RON" }, { numefisier: "2.webp", numeProdus: "Ofiice Profesional" , pret: "350 RON" },
+{ numefisier: "3.webp", numeProdus: "Ofiice Profesional 2019" , pret: "370 RON" }, { numefisier: "4.webp", numeProdus: "Ofiice Profesional", pret: "450 RON"  },
+{ numefisier: "5.webp", numeProdus: "Ofiice Profesional 2021" , pret: "500 RON" }];
+console.log(pozeOffice);
+
+ 
+
+
+
+
+
+
+function creareImagini(obiect, selector) {
+  const inserareImagini = document.querySelector(selector);
+  const elementDom = document.createElement("img");
+
+  const creareDiv = document.createElement("div");
+  creareDiv.classList.add("Produse");
+
+
+
+
+  var elementP = document.createElement("p");
+  elementP.innerHTML = `${obiect.numeProdus}`;
+  creareDiv.appendChild(elementP);
+  var elementP2 = document.createElement("p");
+  var butonCumpara = document.createElement("button");
+  butonCumpara.innerHTML = "Adauga in cos";
+  butonCumpara.style.cursor = "pointer";
+ 
+  
+  elementDom.setAttribute("src", `../produse_office/${obiect.numefisier}`);
+  elementDom.setAttribute("class", selector);
+ 
+  elementP2.innerHTML = "Pret:" + " "  +`${obiect.pret}`;
+  
+
+  creareDiv.appendChild(elementDom);
+  creareDiv.appendChild(elementP2);
+  creareDiv.appendChild(butonCumpara);
+
+  inserareImagini.appendChild(creareDiv);
+
+
+
+
+}
+
+
+
+
+ const pozeTelefon = [{ numefisier: "1a.webp", numeProdus: "Samsung A50" , pret: "1200 RON"  }, { numefisier: "2a.webp", numeProdus: "Huawei Nova 5T", pret: "1400 RON"  },
+{ numefisier: "3a.webp", numeProdus: "Nokia N", pret: "1599 RON"  }, { numefisier: "4a.webp", numeProdus: "Iphone 11", pret: "3000 RON"  },
+{ numefisier: "5a.webp", numeProdus: "Huawei Nova 9", pret: "3400 RON"  }];
+
+const pozeLaptop = [{ numefisier: "1c.webp", numeProdus: "Laptop ASUS X515EA" , pret: "3600 RON"  }, { numefisier: "2c.webp", numeProdus: "Laptop HP ProBook 450 G8", pret: "3700 RON"  },
+{ numefisier: "3c.webp", numeProdus: "Laptop  ASUS Zenbook " , pret: "4299 RON"  }, { numefisier: "4c.webp", numeProdus: "Laptop Acer Aspire", pret: "3100 RON"  },
+{ numefisier: "5c.webp", numeProdus: "Laptop Dell Inspiron 3511 ", pret: "4000 RON"  }];
+
+const pozeBoxe = [{ numefisier: "1d.webp", numeProdus: "Boxe 2.0 Logitech Z120, 1.2W RMS" , pret: "80 RON"  }, { numefisier: "2d.webp", numeProdus: "Boxe Trust Leto 2.0, 6W, Negru Promo", pret: "50 RON"  },
+{ numefisier: "3d.webp", numeProdus: "Boxe 2.0 Logitech S150, 1.2W, negru" , pret: "80 RON"  }, { numefisier: "4d.webp", numeProdus: "Boxe Trust Remo 2.0 , 8W, Negru", pret: "86 RON"  },
+{ numefisier: "5d.webp", numeProdus: "Boxe 2.0 Logitech Z150, 6W RMS, Alb", pret: "137 RON"  }];
+
+window.addEventListener("load", function () {
+  for (let i = 0; i < pozeOffice.length; i++) {
+
+    creareImagini(pozeOffice[i], ".galerie-office");
+  }
+
+  for (let i = 0; i < pozeTelefon.length; i++) {
+
+    creareImagini(pozeTelefon[i], ".galerie-telefoane");
+    creareImagini(pozeLaptop[i], ".galerie-laptop");
+    creareImagini(pozeBoxe[i], ".galerie-boxe");
+  }
+  console.log(document.querySelectorAll(".but"))
+
+});
+
+
